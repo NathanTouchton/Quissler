f5437653ab6e29cea4b2b65f51b3b1121904b241
Nathan Touchton <ntouchton@protonmail.com>
2022-12-27 15:15:35 -0500

    modified:   ui.py

--- ui.py
+++ ui.py
@@ -1,21 +1,28 @@
-from tkinter import Tk,Label,Canvas
+from tkinter import Tk, Label, Canvas, Button, PhotoImage
 
 THEME_COLOR = "#375362"
+def button_filler():
+    pass
 
 class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR)
-        self.score_display = Label(text="Score:0", width=300, height=300, padx=20)
-        self.question_box = Canvas()
+        self.score_display = Label(text="Score:0", bg=THEME_COLOR)
+        self.question_box = Canvas(height=250, width=300)
         self.question_text = self.question_box.create_text(
-            0,
-            0,
+            150,
+            125,
             text="Test",
             font=("Arial", 20, "italic")
         )
-        self.question_box.config(height=250, width=300, padx=20)
-        self.score_display.grid(row=0, column=1)
-        self.question_box.grid(row=1, column=0)
+        self.true_image = PhotoImage(file="images/true.png")
+        self.false_image = PhotoImage(file="images/false.png")
+        self.true_button = Button(image=self.true_image, command=button_filler)
+        self.false_button = Button(image=self.false_image, command=button_filler)
+        self.score_display.grid(row=0, column=1, pady=20)
+        self.question_box.grid(row=1, column=0, padx=20, pady=20, columnspan=2)
+        self.true_button.grid(row=2, column=0, pady=20)
+        self.false_button.grid(row=2, column=1, pady=20)
         self.window.mainloop()
