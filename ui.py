7b16f68dbfa4f246dbc282ddea41fb0c04165922
Nathan Touchton <ntouchton@protonmail.com>
2023-01-05 13:02:16 -0600

    modified:   main.py 	modified:   ui.py

--- ui.py
+++ ui.py
@@ -14,20 +14,20 @@ class QuizInterface:
         self.window.config(bg=THEME_COLOR)
         self.score_display = Label(text="Score:0/10", bg=THEME_COLOR, font=("Arial", 15))
         self.question_box = Canvas(height=250, width=300, bg=WHITE)
-        # self.question_text = self.question_box.create_text(
-        #     150,
-        #     125,
-        #     text=" ",
-        #     font=("Arial", 20, "italic"),
-        #     width=280
-        # )
         self.question_text = self.question_box.create_text(
             150,
             125,
             text=" ",
-            font=("Arial", 11, "italic"),
+            font=("Arial", 20, "italic"),
             width=280
         )
+        # self.question_text = self.question_box.create_text(
+        #     150,
+        #     125,
+        #     text=" ",
+        #     font=("Arial", 11, "italic"),
+        #     width=280
+        # )
         self.true_image = PhotoImage(file="images/true.png")
         self.false_image = PhotoImage(file="images/false.png")
         self.true_button = Button(image=self.true_image, command=self.true_func)
@@ -38,6 +38,13 @@ class QuizInterface:
         self.false_button.grid(row=2, column=1, pady=20)
         self.get_next_question()
         self.window.mainloop()
+    
+    def button_pass(self):
+        pass
+
+    def button_enable(self):
+        self.true_button.configure(command=self.true_func)
+        self.false_button.configure(command=self.false_func)
 
     def true_func(self):
         self.give_feedback(self.quiz.check_answer("true"))
@@ -51,6 +58,8 @@ class QuizInterface:
         self.score_display.configure(text=f"Score:{self.quiz.score}/10")
 
     def give_feedback(self, is_right: bool):
+        self.true_button.configure(command=self.button_pass)
+        self.false_button.configure(command=self.button_pass)
         if is_right is True:
             self.question_box.configure(bg=GREEN)
         elif is_right is False:
@@ -61,5 +70,6 @@ class QuizInterface:
         self.question_box.configure(bg=WHITE)
         if self.quiz.question_number < 10:
             self.get_next_question()
+            self.button_enable()
         elif self.quiz.question_number >= 10:
             self.question_box.itemconfig(self.question_text, text="Game Over")
