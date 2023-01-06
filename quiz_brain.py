f433cd1d297f658d35d902bdb01765c51d7485b4
Nathan Touchton <ntouchton@protonmail.com>
2023-01-05 18:29:08 -0600

    modified:   quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -8,9 +8,6 @@ class QuizBrain:
         self.question_list = q_list
         self.current_question = None
 
-    def still_has_questions(self):
-        return self.question_number < len(self.question_list)
-
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
